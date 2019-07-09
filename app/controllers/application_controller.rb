c33343710b5fa24ed73e5a8038c3a49114b0b1ae
Dennis Chung <chungdennis6@gmail.com>
2019-07-09 15:59:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,8 @@ class ApplicationController < Sinatra::Base
 		erb :login
 	end
 
-	post "/login" douser = User.find_by(:username => params[:username])
+	post "/login" do
+	  user = User.find_by(:username => params[:username])
 
 	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
